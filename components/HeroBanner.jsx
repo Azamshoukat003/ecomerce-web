819b18d6b848f8d42f14bb708a0d70bade9c0a66
AzamShoukat002 <Azamshoukat11225@gmail.com>
2023-08-14 15:52:31 -0700

    ok

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -16,7 +16,7 @@ const HeroBanner = ({ heroBanner }) => {
         />
 
         <div>
-          <Link href={`/product/${heroBanner.product}`}>
+          <Link href={`/`}>
             <button type="button">{heroBanner.buttonText}</button>
           </Link>
 
